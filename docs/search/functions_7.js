338071e55ce0b17f8e5633788b4472b658148ffa
Erriez <Erriez@users.noreply.github.com>
2018-05-21 15:48:15 +0200

    Add missing Doxygen scripts.

---
+++ docs/search/functions_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['tm1638',['TM1638',['../class_t_m1638.html#aac3931b7beb2ad8f5e1c5f3edc5408b6',1,'TM1638']]]
+];
---
+++ docs/search/variables_0.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['_5fbrightness',['_brightness',['../class_t_m1638.html#aad9c8000c101ae90b1f3426be7d168b0',1,'TM1638']]],
+  ['_5fclkpin',['_clkPin',['../class_t_m1638.html#ab4479c9c4104732b75d3dab088e9af85',1,'TM1638']]],
+  ['_5fdiopin',['_dioPin',['../class_t_m1638.html#a2bfb09b35261ee2b0721f62c3c881817',1,'TM1638']]],
+  ['_5fdisplayon',['_displayOn',['../class_t_m1638.html#a62ef809cfcb96e297b4acd1a09cc92e1',1,'TM1638']]],
+  ['_5fstbpin',['_stbPin',['../class_t_m1638.html#a859e7cea7ffd158c13483cf98732c13f',1,'TM1638']]]
+];
