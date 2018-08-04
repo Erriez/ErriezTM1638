e915d832c956f096c6218bd19baef3f6b890556c
Travis CI <travis@travis-ci.org>
2018-08-04 13:00:35 +0000

    Deploy code docs to GitHub Pages Travis build: 25
    Commit: 0980337b8f2b5a8e1f360719c16a5e854063f6c6

--- search/all_4.js
+++ search/all_4.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['end',['end',['../class_t_m1638.html#a73fe114adcfbab4b786641d80b9c6d53',1,'TM1638']]]
+  ['end',['end',['../class_t_m1638.html#a73fe114adcfbab4b786641d80b9c6d53',1,'TM1638']]],
+  ['errieztm1638_2ecpp',['ErriezTM1638.cpp',['../_erriez_t_m1638_8cpp.html',1,'']]],
+  ['errieztm1638_2eh',['ErriezTM1638.h',['../_erriez_t_m1638_8h.html',1,'']]]
 ];
--- search/files_0.js
+++ search/files_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['tm1638_2ecpp',['TM1638.cpp',['../_t_m1638_8cpp.html',1,'']]],
-  ['tm1638_2eh',['TM1638.h',['../_t_m1638_8h.html',1,'']]]
+  ['errieztm1638_2ecpp',['ErriezTM1638.cpp',['../_erriez_t_m1638_8cpp.html',1,'']]],
+  ['errieztm1638_2eh',['ErriezTM1638.h',['../_erriez_t_m1638_8h.html',1,'']]]
 ];
